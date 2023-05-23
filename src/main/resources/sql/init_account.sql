3085e91a5652235ee5f0104ee220e801d9fa3393
Adithya <43409373+adithyamr@users.noreply.github.com>
2023-05-23 20:35:30 +0530

    Update init_account.sql

--- src/main/resources/sql/init_account.sql
+++ src/main/resources/sql/init_account.sql
@@ -1,2 +1,2 @@
 INSERT INTO account (pan, account_number,"password",pin, key) VALUES ('ANWPR1847F','ZM3272','Adithy@1985','271016','ZSIVWGE4ITWC7LQ5T52NGAZARXBHPQRU');
-INSERT INTO account (pan, account_number,"password",pin, key) VALUES ('ALFPA7312P','IMF991','Amith@1986','271016', '');
+INSERT INTO account (pan, account_number,"password",pin, key) VALUES ('ALFPA7312P','IMF991','Amith@1986','271016', 'AOU4RUKWRJEMAFHPNLQUYV5GJDVNYLYK');
